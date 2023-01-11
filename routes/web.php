aafa30057a4babd117732674575b0937554967ed
XuanTuyen <dinhxuantuyen1207@gmail.com>
2023-01-11 16:15:01 +0700

    tesst push

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//sssss
 Route::get('/',[HomeController::class,'index']);
 Route::get('/bai-viet/{id}',[BaiVietController::class,'show']);
 
