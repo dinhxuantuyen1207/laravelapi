69fc71d4861e91aae995516b75713df06ade5faf
XuanTuyen <dinhxuantuyen1207@gmail.com>
2023-01-11 16:29:38 +0700

    tesssss

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,6 @@ Route::get('/bai-viet/{id}',[BaiVietController::class,'show']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\LoginController::class, 'index'])->name('home');
-
+//aaaa
 Route::get('/tim-kiem',[HomeController::class,'search']);
+Route::get('/test',[HomeController::class,'test']);
